00dded7f08e8f7d5bbaf46df3c7573db96fb9481
DESKTOP-0OQH2I3\Jose Andres <lpmayser50@gmail.com>
2021-05-27 22:13:57 -0400

    Initial backend project

---
+++ src/main/java/bo/dynamicworkflow/dynamicworkflowbackend/DynamicWorkflowBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.dynamicworkflow.dynamicworkflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicWorkflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicWorkflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/dynamicworkflow/dynamicworkflowbackend/DynamicWorkflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.dynamicworkflow.dynamicworkflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicWorkflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
